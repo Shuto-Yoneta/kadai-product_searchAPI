1412f666b15eaf51f92725e877f50ba28317e1e0
YS <tokumei.1230@gmail.com>
2021-07-04 00:59:03 +0000

    new project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="center jumbotron" style="background:url(images/bimg001.jpg); background-size:cover;">
+        <div class="text-center">
+            <h1>楽天商品検索サイトへようこそ</h1>
+        </div>
+    </div>
+@endsection
